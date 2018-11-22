52e1af092db963751a406b295b9db028045a395c
Your Name <you@example.com>
2018-11-21 23:35:22 -0500

    Lesson 4.

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by John Huang on 11/21/18.
+//  Copyright © 2018 John Huang. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        }
+    
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by John Huang on 11/21/18.
+//  Copyright © 2018 John Huang. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
